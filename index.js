ee5b17bda4a50c42be59b3a44196ec8158b4e38e
David Pink <dvdpink@gmail.com>
2019-11-05 21:43:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function deleteFromObjectByKey(object, key){
   delete newObj[key]
   return newObj
 }
-destructivelyDeleteFromObjectByKey(object, key){
+function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key];
   return object;
 }
